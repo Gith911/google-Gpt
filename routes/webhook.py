8d2ed46a30be2918929ba3957d0f0b99d86f91b8
Gith911 <123799914+Gith911@users.noreply.github.com>
2024-08-01 23:03:54 +0300

    Update webhook.py

--- routes/webhook.py
+++ routes/webhook.py
@@ -25,4 +25,5 @@ def set_webhook():
 if __name__ == '__main__':
     set_webhook()
     port = int(os.environ.get('PORT', 5000))  # الحصول على المنفذ من متغير البيئة PORT
+    print(f"Running on port {port}")  # تسجيل المنفذ
     app.run(host='0.0.0.0', port=port)  # تأكد من تحديث المنفذ إذا لزم الأمر
