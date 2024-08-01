3c2bbae1d59a6feb48a4c5a41f7695038c790145
Ahmed Alkhaldi <ahmad.abu.gith@gmail.com>
2024-08-02 02:51:38 +0300

    سس

--- config/config.py
+++ config/config.py
@@ -7,9 +7,7 @@ load_dotenv()
 # تعيين مفاتيح API من المتغيرات البيئية
 TELEGRAM_API_KEY = os.getenv('TELEGRAM_API_KEY')
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
-PORT = os.getenv('PORT', 5000)  # تعيين قيمة افتراضية للمنفذ إذا لم يتم تعيينه
 
 # التحقق من القيم
 print(f"TELEGRAM_API_KEY: {TELEGRAM_API_KEY}")
 print(f"GOOGLE_API_KEY: {GOOGLE_API_KEY}")
-print(f"PORT: {PORT}")
